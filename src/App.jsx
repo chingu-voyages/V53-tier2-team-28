c142f2c32d042d835720dd163db45dfb0a7f27ad
minorObsession <terzicbogdan95@gmail.com>
2025-01-24 14:08:32 +0100

    Start working with recipes API

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import Recipes from "./pages/Recipes";
+import RecipePreview from "./UI components/RecipePreview";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="signup" element={<Home />} /> */}
+        <Route path="signup" element={<Home />} />
         <Route path="login" element={<Login />} />
 
-        {/* // ! examples of child-grandchild routes */}
+        <Route path="recipes" element={<Recipes />} />
+        {/*   // ! TESTING PREVIEW */}
+        <Route path="recipes/:id" element={<RecipePreview />} />
+
         {/* <Route path="/child/:id" element={<Child />} />
         <Route path="/child/:id/grand-child" element={<GrandChild />} /> */}
 
---
+++ src/UI components/RecipePreview.jsx
@@ -0,0 +1,5 @@
+function RecipePreview() {
+  return <div></div>;
+}
+
+export default RecipePreview;
