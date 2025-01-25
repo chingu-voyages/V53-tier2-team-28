75cfbe667259219fd44fe929712c6cd6e88619c5
chonger878 <echong06@gmail.com>
2025-01-25 03:05:47 -0800

    Added some placeholder text for signup.jsx file

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 function Signup () {
 
     return <div className="signup-modal">
-        
+        <h1 className=" text-7xl font-bold text-black mx-auto">This is a placeholder text</h1>
+        <button>Submit</button>
     </div>
 }
