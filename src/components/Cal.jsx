2163eb7068228b3bbfd3961bd7a20e3cae8628be
laura <lg8104@gmail.com>
2025-01-26 17:27:44 -0700

    Updating components as I try to resolve tailwind issues so css shows, added mushroom pic to base my potential logo for the app on

--- src/components/Cal.jsx
+++ src/components/Cal.jsx
@@ -1,13 +1,58 @@
 function Cal () {
     return (
         <div className="table">
-            <table className="border-solid">
-                <tr><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr>
-                <tr><td></td><td></td><td></td><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-                <tr><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td></tr>
-                <tr><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td></tr>
-                <tr><td>19</td><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td></tr>
-                <tr><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>31</td><td></td></tr>
+            <table className="border-solid-2 border-gray-950">
+                <tr><th className="border-solid-2 border-gray-950">Sunday</th>
+                <th className="border-solid-2 border-gray-950">Monday</th>
+                <th className="border-solid-2 border-gray-950">Tuesday</th>
+                <th className="border-solid-2 border-gray-950">Wednesday</th>
+                <th className="border-solid-2 border-gray-950">Thursday</th>
+                <th className="border-solid-2 border-gray-950">Friday</th>
+                <th className="border-solid-2 border-gray-950">Saturday</th></tr>
+                <tr><td className="border-solid-2 border-gray-950"></td>
+                <td className="border-solid-2 border-gray-950"></td>
+                <td className="border-solid-2 border-gray-950"></td>
+                <td className="border-solid-2 border-gray-950">1
+                <p>Breakfast: Berry Chia Smoothie & Sourdough Raisin Toast</p>
+                <p>Lunch: Greek Salad & Veggie Primo Pizza</p>
+                <p>Supper: Buffalo Wings & Green Salad</p></td>
+                <td className="border-solid-2 border-gray-950">2
+                <p>Breakfast: Tomato-Feta Spinach Smoothie & Granola</p>
+                <p>Lunch: BLT Sandwich & Cobb Salad</p>
+                <p>Supper: Caesar Salad & Lasagna</p></td>
+                <td className="border-solid-2 border-gray-950">3
+                <p>Breakfast: Peach Mango Pinneaple Smoothie</p>
+                <p>Lunch: Taco Salad</p>
+                <p>Supper: Poutine, Honey Garlic Chicken</p></td>
+                <td className="border-solid-2 border-gray-950">4</td></tr>
+                <tr><td className="border-solid-2 border-gray-950">5</td>
+                <td className="border-solid-2 border-gray-950">6</td>
+                <td className="border-solid-2 border-gray-950">7</td>
+                <td className="border-solid-2 border-gray-950">8</td>
+                <td className="border-solid-2 border-gray-950">9</td>
+                <td className="border-solid-2 border-gray-950">10</td>
+                <td className="border-solid-2 border-gray-950">11</td></tr>
+                <tr><td className="border-solid-2 border-gray-950">12</td>
+                <td className="border-solid-2 border-gray-950">13</td>
+                <td className="border-solid-2 border-gray-950">14</td>
+                <td className="border-solid-2 border-gray-950">15</td>
+                <td className="border-solid-2 border-gray-950">16</td>
+                <td className="border-solid-2 border-gray-950">17</td>
+                <td className="border-solid-2 border-gray-950">18</td></tr>
+                <tr><td className="border-solid-2 border-gray-950">19</td>
+                <td className="border-solid-2 border-gray-950">20</td>
+                <td className="border-solid-2 border-gray-950">21</td>
+                <td className="border-solid-2 border-gray-950">22</td>
+                <td className="border-solid-2 border-gray-950">23</td>
+                <td className="border-solid-2 border-gray-950">24</td>
+                <td className="border-solid-2 border-gray-950">25</td></tr>
+                <tr><td className="border-solid-2 border-gray-950">26</td>
+                <td className="border-solid-2 border-gray-950">27</td>
+                <td className="border-solid-2 border-gray-950">28</td>
+                <td className="border-solid-2 border-gray-950">29</td>
+                <td className="border-solid-2 border-gray-950">30</td>
+                <td className="border-solid-2 border-gray-950">31</td>
+                <td className="border-solid-2 border-gray-950"></td></tr>
             </table>
         </div>
     )
