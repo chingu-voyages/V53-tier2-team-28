c68160141f08a68862392f1e9c6a7fc8e4a7ffd7
laura <lg8104@gmail.com>
2025-01-28 09:01:16 -0700

    Updating css changes in Cal and Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,21 @@ function Footer () {
         <div className="bg-white">
             <div className="link">
                 <a href="https://github.com/v53-tier2-team-28/tree/main">
-                    <img className="color-black h-md w-md" src={github}></img>
+                    <img className="color-black h-sm w-sm" src={github}></img>
                     Chingu Voyage 47 Tier 2 Team 28 Repo
                 </a>
                 <ul>
-                    <li><strong>Developer</strong></li>
-                    <li>Erica C | <a href="https://github.com/chonger878">Github</a> | 
+                    <li className="text-black"><strong>Developer</strong></li>
+                    <li className="text-black">Erica C | <a href="https://github.com/chonger878">Github</a> | 
                     <a href="https://www.linkedin.com/in/charwaeericachong/">LinkedIn</a></li>
-                    <li>Frosty/Laura | <a href="https://github.com/frosty8104">Github</a> | 
+                    <li className="text-black">Frosty/Laura | <a href="https://github.com/frosty8104">Github</a> | 
                     <a href="https://www.linkedin.com/in/laura-gieg-web-designer-dev/">LinkedIn</a></li>
-                    <li>Bogdan Terzic | <a href="https://github.com/minorObsession">Github</a> | 
+                    <li className="text-black">Bogdan Terzic | <a href="https://github.com/minorObsession">Github</a> | 
                     <a href="https://www.linkedin.com/in/bogdanterzic95/">LinkedIn</a></li>
-                    <li><strong>Scrum Master</strong></li>
-                    <li>Noelle Jefferson | <a href="https://github.com/Equillibria">Github</a> | 
+                    <li className="text-black"><strong>Scrum Master</strong></li>
+                    <li className="text-black">Noelle Jefferson | <a href="https://github.com/Equillibria">Github</a> | 
                     <a href="https://www.linkedin.com/in/noelle-jefferson/">LinkedIn</a></li>
-                    <li>Theresa Green | <a href="">Github</a> | <a href="">LinkedIn</a></li>
+                    <li className="text-black">Theresa Green | <a href="">Github</a> | <a href="">LinkedIn</a></li>
                 </ul>
             </div>
         </div>
