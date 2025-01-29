dcdc7f69d65214786fa5a474b4d5a30020823f73
minorObsession <terzicbogdan95@gmail.com>
2025-01-29 05:46:22 +0100

    Add Tailwind to Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslintPlugin from "vite-plugin-eslint";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     eslintPlugin({
       cache: false, // Disable cache if necessary
       include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"], // Include specific files
