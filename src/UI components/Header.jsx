b98f610377cde50fc90e3e2f02e71f2382d362f8
laura <lg8104@gmail.com>
2025-02-06 10:06:37 -0700

    Updating development branch

--- src/UI components/Header.jsx
+++ src/UI components/Header.jsx
@@ -1,7 +1,10 @@
+import Logo from "../assets/mushroom.svg";
+
 function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-background bg-opacity-85 shadow-md p-4 flex items-center z-50">
       {/* // ! LOGO  */}
+      <img className="w-12" src={Logo}></img>
       <div className="ml-4 text-xl font-bold">Logo...</div>
       <p>
         we will likely change color of header and maybe other colors too... to
