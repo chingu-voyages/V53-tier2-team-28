11424de5767591d4cf6a26dd74e5f23d7722b21b
chonger878 <echong06@gmail.com>
2025-02-05 17:06:13 -0800

    updating branch

--- src/UI components/Header.jsx
+++ src/UI components/Header.jsx
@@ -1,10 +1,9 @@
-import Logo from "../assets/mushroom.svg"
+import Mushroom from "../assets/mushroom.svg";
 
 function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-background bg-opacity-85 shadow-md p-4 flex items-center z-50">
-      {/* // ! LOGO  */}
-      <img className= "w-1/3">{Logo}</img>
+      <svg className ="w-3 h-24" xmlns="./assets/mushroom.svg" />
       <p>
         we will likely change color of header and maybe other colors too... to
         match the logo
